d3ed65ede49bc68460b73aeace06289ffb7648b7
dabydat <davidgomezdev@gmail.com>
2024-05-18 22:13:36 -0400

    Probando commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function accionBoton() {
+    document.getElementById("boton-accion").innerHTML = "Boton presionado";
+}
+
+// addEventListener("click", accionBoton)
+
+
+// addEventListener("click", function () {
+//     document.getElementById("boton-accion").innerHTML = "Boton presionado"
+// })
